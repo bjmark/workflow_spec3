9414a73c8f8902659cd85b85f5c4eb57bd42c3fe
pengweimin <bjmark@sina.com>
2014-08-28 09:41:40 +0800

    d

---
+++ workflow_credit_approval.rb
@@ -0,0 +1,71 @@
+# encoding:utf-8
+#
+Ruote.process_definition :code => 'credit_approval', :name => "授信审批",
+  :version => "1.0.0", :target_model => 'Proposal' do
+  cursor do
+    set :field => 'blade', :value => {}
+    set :field => 'blade.helper', :value => 'WorkflowCreditApprovalHelper' 
+
+    business_manager :tag => 'INIT.handler', :name => '主办或协办立项申请'
+    business_dept_head :tag => 'INIT.head_review', :name => '业务负责人审批',
+      :custom_fields => [
+        { :type => 'checkbox', :name => 'declined', :label => '否决' }
+    ]
+
+    jump :to => 'finish', :if => "${f:declined}"
+
+    risk_dept_reviewer :tag => 'INIT.risk_dispatch', :name => '项目复审岗派发',
+      :dispatch_to => { :role => 'risk_dept_examiner' },
+      :before_proceed => {
+      :proceed => 'save_examiner, remind_risk_dept_head | if more_info'
+    }
+
+    risk_dept_examiner :tag => 'INIT.risk_exam', :name => '立项审查',
+      :can_delegate_to => { :role => 'risk_dept_examiner' },
+      :custom_fields => [
+        { :type => 'checkbox', :name => 'more_info', :label => '修改或补充材料' }
+    ]
+    jump :to => 'INIT.handler', :if => '${f:more_info}'
+
+    risk_dept_reviewer :tag => 'INIT.risk_review', :name => '项目复核岗复核'
+
+    risk_dept_head :tag => 'INIT.risk_head_review', :name => '风险负责人审批',
+      :custom_fields => [
+        { :type => 'checkbox', :name => 'declined', :label => '否决' }
+    ]
+    jump :to => 'finish', :if => "${f:declined}"
+
+    business_manager :tag => 'DD.handler', :name => '尽职调查'
+    business_dept_head :tag => 'DD.head_review', :name => '业务部负责人审批'
+
+    risk_dept_reviewer :tag => 'DD.risk_dispatch', :name => '项目复审岗派发',
+      :dispatch_to => { :role => 'risk_dept_examiner' }
+    risk_dept_examiner :tag => 'DD.risk_examine', :name => '项目审查'
+    risk_dept_reviewer :tag => 'DD.risk_review', :name => '项目复核岗复审'
+
+    risk_dept_head :tag => 'VOTE.risk_head', :name => '负责人审批',
+      :custom_fields => [
+        { :type => 'checkbox', :name => 'declined', :label => '否决' },
+        { :type => 'checkbox', :name => 'more_info', :label => '业务经理修改或补充材料' }
+    ]
+    jump :to => 'finish', :if => "${f:declined}"
+    jump :to => 'DD.handler', :if => '${f:more_info}'
+
+    committee_secretary :tag => 'VOTE.collect_votes', :name => '评审意见汇总',
+      :more_info => 'DD.handler'
+
+    risk_dept_reviewer :tag => 'VOTE.risk_review', :name => '项目复审岗复审'
+    risk_dept_head :tag => 'VOTE.risk_head', :name => '风险负责人审批'
+
+    # VP
+    committee_director :tag => 'VOTE.review', :name => '主任委员审批',
+      :custom_fields => [
+        { :type => 'checkbox', :name => 'president_approval', :label => '总裁审批'}
+    ]
+    president :tag => 'VOTE.president', :name => '总裁审批', :if => '${f:president_approval}'
+
+    risk_dept_review :tag => 'VOTE.notice'
+    risk_dept_head :tag => 'VOTE.notice_dispatch'
+    completer :tag => 'finish', :name => '结束'
+  end
+end
---
+++ workflow_credit_approval_helper.rb
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+class WorkflowCreditApprovalHelper < WorkflowHelper
+  def save_examiner
+    #WorkflowLog.create!(:message => "save_examiner")
+    @workitem.fields['blade']['default_examiner_id'] = @req.params['dispatch_to']
+    @workitem.fields['blade']['receiver_id'] = @req.params['dispatch_to']
+  end
+
+  def remind_risk_dept_head
+    WorkflowLog.create!(:message => "remind_risk_dept_head")
+  end
+
+  def save_examiner_suggest
+    suggest = @req.params['suggest']
+    trans = {
+      'aggree' => '同意',
+      'deny' => '否决',
+      'aggree_with_condition' => '有条件同意',
+      'suspend' => '暂缓'
+    }
+    comments = "#{trans[suggest]}: #{@req.params[suggest]}"
+    @req.instance_variable_set('@comments', comments)
+  end
+
+  def set_receiver
+    @workitem.fields['blade']['receiver_id'] = 
+      @workitem.fields['blade']['default_examiner_id']
+  end
+
+  def validate_examiner
+    error = []
+    if @req.params['examiner_id'] == ''
+      error << '请指定审查人'
+    end
+    error
+  end
+
+  def validate_suggest
+    error = []
+    if @req.params['suggest'].nil?
+      error << '请输入审查建议'
+    end
+    error
+  end
+
+  def save_final_decision_maker
+    if @workitem.fields['blade']['final_decision_maker_role'] != @req.params['final_decision_maker_role']
+      @workitem.fields['blade']['final_decision_maker_role_history'] = [] unless @workitem.fields['blade']['final_decision_maker_role_history']
+      @workitem.fields['blade']['final_decision_maker_role_history'] <<
+      "#{Role.where(:code => @workitem.participant_name).first.name}设置终审人为:#{Role.where(:code => @req.params['final_decision_maker_role']).first.name}" 
+    end
+
+    @workitem.fields['blade']['final_decision_maker_role'] = @req.params['final_decision_maker_role']
+    case @workitem.fields['blade']['final_decision_maker_role']
+    when 'committee_director'
+      @workitem.fields['blade']['committee_director1'] = { "下一步:总裁审批" => 'del' }
+    when 'president'
+      @workitem.fields['blade']['committee_director1'] = { 
+        "同意" => 'del',
+        "否决" => 'del'
+      }
+    end
+  end
+end
+
